946190e27272a27d813ebb8c031ef3eab6910012
HYUN <114287255+Creativerjongs@users.noreply.github.com>
2025-08-10 03:46:19 +0900

    Observer-part2

--- src/active_menu.js
+++ src/active_menu.js
@@ -12,8 +12,12 @@ const sectionIds = ['#home', '#about', '#skills', '#work', '#testimonial', '#con
 const sections = sectionIds.map(id => document.querySelector(id));
 const navItems = sectionIds.map(id => document.querySelector(`[href="${id}"]`));
 const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
 
-const options = {};
+const options = {
+    rootMargin: '-20% 0px 0px 0px',
+    threshold: [0, 0.98],
+};
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach(section => observer.observe(section));
 
@@ -22,13 +26,11 @@ function observerCallback(entries){
     entries.forEach(entry => {
         const index = sectionIds.indexOf(`#${entry.target.id}`);
         visibleSections[index] = entry.isIntersecting;
-        selectLastOne = index === sectionIds.length - 1 && entry.isIntersecting && entry.intersectionRatio >= 0.99;
+        selectLastOne = index === sectionIds.length - 1 && entry.isIntersecting && entry.intersectionRatio >= 0.95;
     });
-    console.log(visibleSections);
-    console.log('무조건 라스트 섹션!!!!', selectLastOne);
 
     const navIndex = selectLastOne ? sectionIds.length - 1 :findFirstIntersecting(visibleSections);
-    console.log(sectionIds[navIndex]);
+    selectNavItem(navIndex);
 }
 
 function findFirstIntersecting(intersections){
@@ -36,3 +38,10 @@ function findFirstIntersecting(intersections){
     return index >= 0 ? index : 0;
 }
 
+function selectNavItem(index){
+    const navItem = navItems[index];
+    if(!navItem) return;
+    activeNavItem.classList.remove('active');
+    activeNavItem = navItem;
+    activeNavItem.classList.add('active');
+}
